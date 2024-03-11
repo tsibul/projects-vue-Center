0f3b00e645b859484fa46469eb4136a1e78afd0f
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-03-11 03:47:14 +0300

    receive check event

--- src/components/settings/SettingsRightComponent.vue
+++ src/components/settings/SettingsRightComponent.vue
@@ -1,17 +1,78 @@
 <script setup>
 
+import SettingsSingleComponent from "@/components/settings/SettingsSingleComponent.vue";
 </script>
 
 <template>
-
+  <div class="dict-right">
+    <SettingsSingleComponent v-for="item in dictionariesReady" :key="item.id"
+                             :style="showState[item]['checked'] ? 'display:flex' : 'display:none'"
+                             :dictionaryName="item" :icon="showState[item]['icon']" :label="showState[item]['label']"
+    />
+  </div>
 </template>
 
 <script>
+
 export default {
-  name: 'SettingsRightComponent'
+  name: 'SettingsRightComponent',
+  data() {
+    return {
+      showState: {},
+      dictionariesReady: []
+    }
+  },
+  props: {
+    checkedData: Object,
+  },
+  computed: {
+    fieldsData() {
+      return this.$store.getters.getFieldData;
+    },
+  },
+  created() {
+    this.showDictState();
+    console.log(this.checkedData)
+  },
+  watch: {
+    checkedData: {
+      handler: 'handleCheckedDataChange',
+      deep: true,
+    },
+  },
+  methods: {
+    showDictState() {
+      Object.keys(this.fieldsData).forEach((key) => {
+        this.showState[key] = {'checked': false, 'ready': false};
+      });
+    },
+    handleCheckedDataChange(newCheckedData) {
+      const {dictionaryName, dictionaryLabel, icon, checked} = newCheckedData;
+      const state = this.showState[dictionaryName]['ready'];
+
+      if (checked && !state) {
+        this.showState[dictionaryName] = {'checked': true, 'ready': true, 'label': dictionaryLabel, 'icon': icon};
+        this.dictionariesReady.push(dictionaryName);
+        this.dictionariesReady.sort()
+      } else this.showState[dictionaryName]['checked'] = !(!checked && state);
+    },
+  },
 }
 </script>
 
 <style scoped lang="scss">
+
+.dict-right {
+  position: relative;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
+  grid-auto-flow: dense;
+  gap: 20px;
+  width: 100%;
+}
+
+.hidden{
+  display: none;
+}
 
 </style>
