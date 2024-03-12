d0f99156724ee1ef724f621f3d0aa5c7e6b0c00f
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-03-13 02:55:39 +0300

    removed cursor pointer

--- src/components/settings/TitleRowComponent.vue
+++ src/components/settings/TitleRowComponent.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="dict-block__row active dict-block__row_right-margin" :class="rowClass">
+  <div class="block__row active block__row_right-margin" :class="rowClass">
     <div v-for="fieldName in fieldsData[dictionaryName]" :key="fieldName.id">{{ fieldName.label }}</div>
     <button class="btn btn-save" @click="newRecord">добавить</button>
   </div>
@@ -32,13 +32,12 @@ export default {
 <style scoped lang="scss">
 @import "@/assets/maket/scss/dictionary_sizes";
 
-.dict-block__row {
+.block__row {
   display: grid;
   align-items: center;
   gap: 10px;
   padding: 8px 8px;
   border-radius: 10px;
-  cursor: pointer;
   flex-wrap: nowrap;
 
   &_right-margin {
