ba7f70a8b92d9a197256c398d4b0bbf3787c6d83
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-03-16 22:17:09 +0300

    finished with interactive search

--- src/components/form_fields/DropDownComponent.vue
+++ src/components/form_fields/DropDownComponent.vue
@@ -4,22 +4,105 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 
 <template>
   <div class="dropdown report_dropdown dropdown_dict">
-    <input type="text" class="dropdown__hidden" :name="fieldName" :value="fieldValue">
-    <div class="dropdown__input-block">
-      <input type="text" class="dropdown__input dropdown__input_dict" placeholder="Поиск.." :value="fieldValue">
-      <font-awesome-icon :icon="['fas', 'angle-down']" />
+    <input type="text" class="dropdown__hidden"
+           :name="fieldName"
+           :value="currentId">
+    <div class="dropdown__input-block"
+         @click.stop="toggleDropdown">
+      <input type="text" class="dropdown__input dropdown__input_dict" placeholder="Поиск.."
+             v-model="searchString"
+             @input="validateField"
+             :class="field['readonly'] ? 'inactive' : null"
+             :readonly="field['readonly']"
+      >
+      <font-awesome-icon :icon="['fas', 'angle-down']"/>&nbsp;
     </div>
-    <ul class="dropdown__content">
+    <ul class="dropdown__content" v-show="showDropdown">
+      <li v-for="option in optionList"
+          v-show="!searchString || option[Object.keys(option)[0]].toUpperCase().indexOf(searchString.toUpperCase()) > -1"
+          :key="option"
+          @click="selectOption(option)">
+        {{ option[Object.keys(option)[0]] }}
+      </li>
     </ul>
   </div>
 </template>
 <script>
+import {fetchData} from "@/components/services/fetchData.js";
+
 export default {
   name: 'DropDownComponent',
+  inject: ['appUrl'],
+  emits: ['field-valid'],
+  data() {
+    return {
+      showDropdown: false,
+      optionList: [],
+      currentValue: null,
+      currentId: null,
+      searchString: this.currentValue,
+      selectLine: false,
+      fieldName: this.field['field']
+    }
+  },
+  watch: {
+    searchString(newValue) {
+      if (newValue) {
+        if (!this.selectLine) {
+          this.showDropdown = true;
+          this.selectLine = false;
+        }
+      }
+    }
+  },
   props: {
-    fieldName: String,
     fieldValue: String,
-    foreignClass: String
+    field: Object
+  },
+  methods: {
+    toggleDropdown() {
+        this.showDropdown = !this.showDropdown;
+    },
+    selectOption(option) {
+        this.currentId = Object.keys(option)[0]
+        this.searchString = '';
+        this.searchString = option[this.currentId];
+        this.showDropdown = false;
+        this.selectLine = true;
+        this.$emit('field-valid', {
+          'fieldName': this.fieldName,
+          'result': true
+        });
+    },
+    async createOptionList() {
+      const tokenName = 'maketUserToken';
+      const optionUrl = this.appUrl + `dictionary_filter/${this.foreignClass}/default/0`;
+      this.optionList = await fetchData(optionUrl, tokenName)
+    },
+    validateField() {
+      let inputResult = false;
+      const expectedValue = this.optionList.find(obj => Object.prototype.hasOwnProperty.call(obj, this.currentId));
+      if (expectedValue === this.currentValue) {
+        inputResult = true;
+      }
+      this.$emit('field-valid', {
+        'fieldName': this.fieldName,
+        'result': inputResult
+      });
+    }
+  },
+  created() {
+    (async () => {
+      await this.createOptionList();
+    })();
+    this.currentValue = this.fieldValue;
+    // this.fieldName = this.field['field'];
+
+  },
+  computed: {
+    foreignClass() {
+      return this.field['foreignClass']
+    },
   }
 }
 </script>
@@ -29,7 +112,7 @@ export default {
 
 .dropdown {
   position: relative;
-  height: 40px;
+  //height: 40px;
 
   &__content {
     width: 100%;
@@ -38,7 +121,7 @@ export default {
     @include brd-standard;
     position: absolute;
     background-color: $colorSecondary100;
-    display: none;
+    //display: none;
     z-index: 7;
 
     & li {
@@ -48,7 +131,7 @@ export default {
 
       &:hover {
         color: $colorSecondary100;
-        background-color: $colorPrimary800;
+        background-color: $colorPrimary500;
       }
     }
   }
