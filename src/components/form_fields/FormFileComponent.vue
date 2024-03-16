391d1bc5f0350b08846ba44849e4b64873c6312e
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-03-16 22:15:29 +0300

    initial commit

--- src/components/form_fields/FormFileComponent.vue
+++ src/components/form_fields/FormFileComponent.vue
@@ -1,13 +1,16 @@
 <script setup>
 
 </script>
-
 <template>
-
+  <input class="form-input" type="file" :name="fieldName" :value="fieldValue">
 </template>
 <script>
 export default {
   name: 'FormFileComponent',
+  props: {
+    fieldName: String,
+    field: Object
+  }
 }
 </script>
 <style scoped lang="scss">
