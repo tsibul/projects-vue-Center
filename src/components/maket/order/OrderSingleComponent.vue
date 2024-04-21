40c55412462a604ce3eb09292a99195f025b20f8
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-04-22 02:00:07 +0300

    emit delete alert, separate style for deleted order

--- src/components/maket/order/OrderSingleComponent.vue
+++ src/components/maket/order/OrderSingleComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <details>
     <summary class="order-summary"
-             :class="order.maket_status"
+             :class="order.deleted ? 'deleted': order.maket_status"
              :data-id="order.pk">
       <div class="to-check">{{ order.to_check ? '?' : '' }}</div>
       <div class="active">{{ order.maket_status }}</div>
@@ -10,11 +10,19 @@
       <div>{{ order.our_company__code }}</div>
       <div class="customer_name">{{ order.customer__name }}</div>
       <div>{{ order.manager__name }}</div>
-      <div class="mail">{{ order.manager__phone }}&nbsp;{{order.manager__mail}}</div>
-      <button class="btn btn-neutral">макет</button>
+      <div class="mail">{{ order.manager__phone }}&nbsp;{{ order.manager__mail }}</div>
+        <button class="btn btn-neutral"
+                v-if="!order.deleted"
+        >макет
+        </button>
       <button class="btn btn-save">
-        <font-awesome-icon :icon="['fas', 'arrow-up-from-bracket']" class="fa"/>&nbsp;0&nbsp;/&nbsp;0</button>
-      <button class="btn btn-close">удалить</button>
+        <font-awesome-icon :icon="['fas', 'arrow-up-from-bracket']" class="fa"/>&nbsp;0&nbsp;/&nbsp;0
+      </button>
+      <button class="btn btn-close"
+              @click="deleteAlert"
+              v-if="!order.deleted"
+      >удалить
+      </button>
     </summary>
     <OrderItemComponent
         :orderId="order.pk"
@@ -28,13 +36,17 @@ import OrderItemComponent from "@/components/maket/order/OrderItemComponent.vue"
 export default {
   name: "OrderSingleComponent",
   components: {OrderItemComponent},
+  emits: ['delete-alert'],
   data() {
     return {
-      orderDateLocal: null
+      orderDateLocal: null,
+      deleteUrl: null,
+      showDeleteAlert: false,
     }
   },
   created() {
     this.orderDateLocal = this.reformatDate();
+    this.deleteUrl = `delete_order/${this.order.pk}`;
   },
   props: {
     order: Object
@@ -43,8 +55,11 @@ export default {
     reformatDate() {
       const date = this.order.order_date.split('-')
       return `${date[2]}.${date[1]}.${date[0].slice(-2)}`
-    }
-  }
+    },
+    deleteAlert() {
+      this.$emit('delete-alert', this.deleteUrl);
+    },
+  },
 }
 </script>
 
@@ -52,7 +67,7 @@ export default {
 @import "@/assets/maket/scss/vars";
 @import "@/assets/maket/scss/mixins";
 
-details[open]{
+details[open] {
   background-color: $colorPrimary200;
 }
 
@@ -90,12 +105,16 @@ details[open]{
   color: $colorNeutral600;
 }
 
-.mail{
+.mail {
   word-break: break-all;
 }
 
-.customer_name{
+.customer_name {
   text-wrap: normal;
 }
 
+.deleted {
+  opacity: 0.5;
+  background-color: $colorPrimary200;
+}
 </style>
