d818170f9fab8fc091e108ec50d6e097251216c6
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-04-23 07:44:50 +0300

    drag & drop separate mixin

---
+++ src/components/modal_drag_drop/modalDragAndDrop.js
@@ -0,0 +1,31 @@
+export const modalDragAndDrop = {
+    data() {
+        return {
+            dragging: false,
+            diffX: 0,
+            diffY: 0,
+        }
+    },
+    methods: {
+        startDrag(event) {
+            this.dragging = true;
+            const modal = this.$refs.modalDraggable;
+            const rect = modal.getBoundingClientRect();
+            this.diffX = event.clientX - rect.left;
+            this.diffY = event.clientY - rect.top;
+        },
+        stopDrag() {
+            this.dragging = false;
+        },
+        drag(event,) {
+            if (this.dragging) {
+                const modal = this.$refs.modalDraggable;
+                const x = event.clientX - this.diffX;
+                const y = event.clientY - this.diffY;
+                modal.style.left = `${x}px`;
+                modal.style.top = `${y}px`;
+            }
+        },
+
+    }
+}
