5fdb7ab27c6478242191ca33d26b29a6df6098d3
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-04-29 00:02:08 +0300

    added heading

--- src/components/maket/files/FilesComponent.vue
+++ src/components/maket/files/FilesComponent.vue
@@ -1,10 +1,43 @@
-<script setup>
-
-</script>
-
 <template>
-<div>Files</div>
+  <ClauseHeadingComponent
+      :title="'Файлы'"
+      :icon="'file'"
+      :font-class="{'font__normal': true}"
+      :check-box-deleted="true"
+      :check-box-deleted-text="'скрыть не удаленные'"
+      @search-string="search"
+  />
 </template>
+
+<script >
+import ClauseHeadingComponent from "@/components/maket/ClauseHeadingComponent.vue";
+
+export default {
+  name: "FilesComponent",
+  components: {ClauseHeadingComponent},
+  inject: ['appUrl', 'tokenName'],
+  data() {
+    return {
+      dictionaryName: 'Pattern',
+      searchInput: '',
+      searchString: 'default',
+      order: 'default',
+    }
+  },
+  methods: {
+    search() {
+      if (this.searchInput !== '') {
+        this.searchString = this.searchInput.replace(' ', '_');
+      } else {
+        this.searchString = 'default';
+      }
+    },
+    clearInput() {
+      this.searchInput = '';
+    },
+  },
+}
+</script>
 
 <style scoped lang="scss">
 
