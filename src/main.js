0264df0ed25abf10cfb8a59ee03a1553aefc315e
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-04-29 18:29:31 +0300

    added fa regular icons

--- src/main.js
+++ src/main.js
@@ -10,8 +10,10 @@ import store from './store'; // Импорт хранилища Vuex
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 
 library.add(fas)
+library.add(far)
 
 const app = createApp(App)
 
