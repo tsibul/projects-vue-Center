5eb9cc251e0653b2116c907d25bdf8257b0cb773
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-05-01 08:12:00 +0300

    separate route for maket_layout

--- src/router/index.js
+++ src/router/index.js
@@ -2,22 +2,30 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/maket',
-      name: 'maket',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/MaketApp.vue')
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/maket',
+            component: () => import('../views/MaketApp.vue'),
+            children: [
+                {
+                    path: '', // Пустой путь для отображения MaketApp.vue
+                    name: 'maket',
+                    component: () => import('../views/MaketApp.vue')
+                },
+                {
+                    path: 'layout', // Путь layout относится к родительскому пути /maket
+                    name: 'maket_layout',
+                    component: () => import('../components/maket/maket_layout/MaketLayoutComponent.vue')
+                }
+            ]
+        }
+    ]
 })
 
 export default router
