dfefc50ce3849d07dc80f0672d9ab9d738587200
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-05-01 08:14:55 +0300

    show A4 layout (no printable)

---
+++ src/components/maket/maket_layout/A4MarkingComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="frame-set">
+    <div class="a4-marking no-print"></div>
+    <div class="a4-marking no-print"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "A4MarkingComponent"
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/maket/scss/vars';
+
+.frame-set {
+  position: absolute;
+  display: flex;
+}
+
+.a4-marking {
+  height: 297mm;
+  width: 210mm;
+  margin: 1px 0 0 1px;
+  border: 1px solid $colorSecondary400;
+}
+</style>
