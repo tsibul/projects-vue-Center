65551b1a12957114be8f2e397fdea29aee1a7dbc
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-05-04 00:41:36 +0300

    initial commit for grouping added event and button for grouping header fixed blank header added for correct prining

--- src/components/maket/maket_layout/TechDataComponent.vue
+++ src/components/maket/maket_layout/TechDataComponent.vue
@@ -1,14 +1,21 @@
 <template>
+  <div class="no-print blank-header"></div>
   <header class="tech-data no-print">
     <button class="btn btn-close">закрыть</button>
     <button class="btn btn-save">печать</button>
     <div></div>
     <button class="btn btn-neutral"
             @click="togglePictures"
-    >показать&nbsp;/&nbsp;скрыть&nbsp;картинки</button>
+    >показать&nbsp;/&nbsp;скрыть&nbsp;картинки
+    </button>
     <button class="btn btn-neutral"
             @click="toggleFrame"
-    >показать&nbsp;/&nbsp;скрыть&nbsp;рамки</button>
+    >показать&nbsp;/&nbsp;скрыть&nbsp;рамки
+    </button>
+    <button class="btn btn-neutral"
+            @click="showSort"
+    >сортировка
+    </button>
     <div></div>
     <input type="number"
            class="form-input"
@@ -27,17 +34,17 @@
 
 export default {
   name: "TechDataComponent",
-  emits: ['show-pictures', 'show-frame'],
+  emits: ['show-pictures', 'show-frame', 'show-sort'],
   props: {
     maketId: String,
   },
-  data () {
-    return{
+  data() {
+    return {
       showPictures: false,
       showFrame: true
     }
   },
-  methods:{
+  methods: {
     togglePictures() {
       this.showPictures = !this.showPictures;
       this.$emit('show-pictures', this.showPictures);
@@ -46,19 +53,31 @@ export default {
       this.showFrame = !this.showFrame;
       this.$emit('show-frame', this.showFrame);
     },
+    showSort() {
+      this.$emit('show-sort')
+    }
   }
 }
 </script>
 
 
 <style scoped lang="scss">
+@import "@/assets/maket/scss/vars";
 .tech-data {
   display: flex;
   padding: 12px;
   gap: 12px;
+  position: fixed;
+  top: 0;
+  background-color: $colorPrimary100;
+  z-index: 3;
+  width: 100%;
 }
 
-.long-input{
+.long-input {
   width: 100%;
+}
+.blank-header{
+  height: 63px;
 }
 </style>
