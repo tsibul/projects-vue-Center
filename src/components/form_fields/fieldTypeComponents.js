2d60b0f655b1899f09abbcc0b6edef3e860bce69
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-05-05 23:25:09 +0300

    solve problem with fieldValue input type

--- src/components/form_fields/fieldTypeComponents.js
+++ src/components/form_fields/fieldTypeComponents.js
@@ -5,7 +5,6 @@ import DropDownComponent from "@/components/form_fields/DropDownComponent.vue";
 import FormBooleanComponent from "@/components/form_fields/FormBooleanComponent.vue";
 import FormChoicesComponent from "@/components/form_fields/FormChoicesComponent.vue";
 import FormFileComponent from "@/components/form_fields/FormFileComponent.vue";
-import FormInputNumberComponent from "@/components/form_fields/FormInputNumberComponent.vue";
 
 export const fieldTypeComponent = {
     'string': {
