4b93bf611e81197522bb9a9dbd52c481567662ca
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-05-06 08:56:08 +0300

    initial commit

---
+++ src/components/maket/maket_layout/layout_content/ContentFrameComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="content-frame"
+  >
+    {{ groupMame }}
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "ContentFrameComponent",
+  props: {
+    groupData: Array,
+    groupMame: String,
+  },
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/maket/scss/vars";
+
+.content-frame {
+  width: 200mm;
+  margin: 5mm 5mm 0 5mm;
+}
+</style>
