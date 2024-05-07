d7b32ee6a4cc809cc1ce87b903e586a5fd83b879
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-05-07 03:17:14 +0300

    existing patterns components initial commit

---
+++ src/components/maket/maket_layout/layout_content/PenFrameComponent.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "PenFrameComponent"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/maket/maket_layout/layout_content/SingleDetailFrameComponent.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: "SingleDetailFrameComponent"
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
