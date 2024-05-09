afd489958cd3790d545010c44c0ab175cc87ff6f
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-05-09 17:52:19 +0300

    initial commit

--- src/components/maket/maket_layout/layout_content/PenFrameComponent.vue
+++ src/components/maket/maket_layout/layout_content/PenFrameComponent.vue
@@ -1,13 +1,51 @@
+<template>
+  <div class="pen-frame">
+    <div class="pen-frame_small">
+      <SinglePenSmallSingleComponent
+          v-for="pen in groupData"
+          :key="pen.id"
+          :pen-data="pen"
+          :pen-images="groupImages"
+      />
+      <div class="pen-frame__big">
+
+      </div>
+    </div>
+  </div>
+</template>
+
 <script>
+import SinglePenSmallSingleComponent
+  from "@/components/maket/maket_layout/layout_content/SinglePenSmallComponent.vue";
+
 export default {
-  name: "PenFrameComponent"
+  name: "PenFrameComponent",
+  components: {SinglePenSmallSingleComponent},
+  data() {
+    return {}
+  },
+  props: {
+    groupData: Array,
+    groupImages: Array
+  },
 }
 </script>
 
-<template>
-
-</template>
-
 <style scoped lang="scss">
+@import "@/assets/maket/scss/vars";
+
+.pen-frame {
+  display: flex;
+  width: 100%;
+
+  &__big {
+    //width: 150mm;
+  }
+
+  &_small {
+    width: 100%
+  }
+
+}
 
 </style>
