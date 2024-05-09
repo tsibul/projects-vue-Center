afc9830879bae03bc400e4ff49bd68bd245a5a56
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-05-09 17:51:54 +0300

    set images color to html template

---
+++ src/components/maket/maket_layout/layout_content/setImageColors.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import {fetchData} from "@/components/services/fetchData.js";
+
+export async function setImageColors(article, html, appUrl, token) {
+    const hexUrl = `${appUrl}item_color_code_list/${article}`
+    const response = await fetchData(hexUrl, token)
+    const articleColors = response.id
+    for (let i = 0; i < articleColors.length; i++){
+        const j = i + 1;
+        html = html.replace('itemColor_' + j, articleColors[i]);
+    }
+    return html;
+}
