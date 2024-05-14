308a838907684d3496af4e6ddb4283608ac9e9a6
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-05-14 21:14:18 +0300

    show miniature toggleMiniature true/false add/remove .id-hidden

--- src/components/maket/maket_layout/layout_content/ContentFrameComponent.vue
+++ src/components/maket/maket_layout/layout_content/ContentFrameComponent.vue
@@ -10,17 +10,21 @@ export default {
     groupName: String,
     groupPatternName: String,
   },
-  data(){
-    return{
+  data() {
+    return {
       selectedComponent: goodsLayout[this.groupPatternName] ? goodsLayout[this.groupPatternName] : null,
       currentGroupData: this.groupData,
+      showMiniature: true,
     }
   },
-  methods:{
-    positionSelected(groupData){
+  methods: {
+    positionSelected(groupData) {
       this.currentGroupData = groupData;
       this.$emit("position-selected", [this.groupName, this.currentGroupData]);
-    }
+    },
+    toggleMiniature(){
+      this.showMiniature = !this.showMiniature;
+    },
   },
 }
 </script>
@@ -29,30 +33,61 @@ export default {
   <div class="content-frame"
        v-if="selectedComponent"
   >
-    <div class="content-frame__header">{{ groupName.replace('()', ' ') }}</div>
+    <div class="content-frame__frame">
+      <div class="content-frame__header">{{ groupName.replace('()', ' ') }}</div>
+      <div class="content-frame__header_end no-print">
+        <input type="checkbox"
+               class="check"
+               checked
+               :id="groupName"
+               @change="toggleMiniature"
+        >
+        &nbsp;
+        <label :for="groupName" >показывать миниатюры</label>
+      </div>
+    </div>
     <component
         v-if="selectedComponent"
         :is="selectedComponent"
         :group-data="currentGroupData"
         :group-images="groupImages"
-        @position-selected="positionSelected"    />
+        @position-selected="positionSelected"
+        :show-miniature="showMiniature"
+    />
   </div>
 </template>
 
 <style scoped lang="scss">
 @import "@/assets/maket/scss/vars";
+@import "@/assets/maket/scss/mixins";
 
 .content-frame {
   border-top: 1px solid $colorPrimary200;
   width: 200mm;
   margin: 5mm 5mm 6px 5mm;
   position: relative;
-  &__header{
+
+  &__frame {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
     font-size: 12px;
     position: absolute;
-    background-color: #fff;
-    left: 32px;
+    //left: 20mm;
     top: -9px;
+    width: 100%;
+    padding: 0 32px;
+  }
+
+  &__header {
+    background-color: #fff;
+    padding: 0 12px;
+  }
+
+  &__header_end {
+    display: flex;
+    align-items: center;
+    background-color: #fff;
     padding: 0 12px;
   }
 }
--- src/components/maket/maket_layout/layout_content/pen_component/PenFrameComponent.vue
+++ src/components/maket/maket_layout/layout_content/pen_component/PenFrameComponent.vue
@@ -18,7 +18,8 @@ export default {
   },
   props: {
     groupData: Array,
-    groupImages: Array
+    groupImages: Array,
+    showMiniature: Boolean
   },
   methods: {
     positionSelected(penData) {
@@ -37,11 +38,11 @@ export default {
       if (hexColor) {
         square.style.backgroundColor = hexColor;
       }
-      for(let itemIndex in this.currentGroupData){
+      for (let itemIndex in this.currentGroupData) {
         let item = this.currentGroupData[itemIndex];
-        for(let printItemIndex in item.print_item){
+        for (let printItemIndex in item.print_item) {
           let printItem = item.print_item[printItemIndex];
-          if(printItem.place === place){
+          if (printItem.place === place) {
             printItem.color[index]['pantone'] = pantone;
           }
         }
@@ -60,18 +61,18 @@ export default {
            :key="item.id"
       >
         <div v-if="bigImages[item]">
-          <div
-              v-html="bigImages[item][0].replace('itemWidth', bigImages[item][3])"
+          <div class="pen-frame__big_center"
+               v-html="bigImages[item][0].replace('itemWidth', bigImages[item][3])"
           ></div>
           <div class="pen-frame__big_center">расположение:&nbsp;{{ bigImages[item][2] }}</div>
         </div>
       </div>
-      <div  class="pen-frame__big_footer"
+      <div class="pen-frame__big_footer"
            v-for="(printItem, index) in currentGroupData[0].print_item"
            :key="index">
-        <div><b>{{ printItem.position }}</b>&nbsp;<small>({{printItem.place}})</small></div>
+        <div><b>{{ printItem.position }}</b>&nbsp;<small>({{ printItem.place }})</small></div>
         <div class="pen-frame__big_footer"
-            v-for="(color, index) in printItem.color"
+             v-for="(color, index) in printItem.color"
              :key="index"
         >
           <div>цвет</div>
@@ -85,7 +86,9 @@ export default {
         </div>
       </div>
     </div>
-    <div class="pen-frame__small">
+    <div class="pen-frame__small"
+         :class="!showMiniature ? 'id-hidden' : ''"
+    >
       <SinglePenSmallSingleComponent
           v-for="(pen, index) in currentGroupData"
           :key="index"
@@ -103,11 +106,13 @@ export default {
 @import "@/assets/maket/scss/mixins";
 
 .pen-frame {
-  @include d-flex-center(space-between);
+  display: flex;
+  justify-content: space-between;
   width: 100%;
 
   &__big {
     padding: 12px 0;
+    width: 100%;
 
     &_center {
       text-align: center;
@@ -134,8 +139,7 @@ export default {
   }
 
   &__small {
-    //width: 100%
-  }
+margin-top: 4px  }
 
 }
 
