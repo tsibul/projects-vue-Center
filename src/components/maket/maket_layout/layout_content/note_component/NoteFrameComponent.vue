5f2881c5908fdb385c34fcad8fa82745848ac6ba
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-05-19 14:11:41 +0300

    note frame

--- src/components/maket/maket_layout/layout_content/note_component/NoteFrameComponent.vue
+++ src/components/maket/maket_layout/layout_content/note_component/NoteFrameComponent.vue
@@ -1,13 +1,174 @@
 <script>
+import {fetchData} from "@/components/services/fetchData.js";
+import SingleNoteSmallComponent
+  from "@/components/maket/maket_layout/layout_content/note_component/SingleNoteSmallComponent.vue";
+
 export default {
-  name: "NoteFrameComponent"
+  name: "NoteFrameComponent",
+  components: {SingleNoteSmallComponent},
+  inject: ['tokenName', 'appUrl'],
+  data() {
+    return {
+      currentGroupData: this.groupData,
+      bigImages: {},
+      orientationResult: null,
+    }
+  },
+  props: {
+    groupData: Array,
+    groupImages: Array,
+    showMiniature: Boolean,
+    selectAll: Boolean,
+    totalOrientation: Boolean,
+  },
+  methods: {
+    printItemChecked(itemData) {
+      this.bigImages[itemData[0]] = itemData[1]
+    },
+    async changeColor(event, place, index) {
+      const pantone = event.target.value;
+      const square = event.target.closest('.pen-frame__big_footer').querySelector('.pen-frame__big_square');
+      const hexUrl = `${this.appUrl}hex_from_pantone/${pantone}`;
+      const data = await fetchData(hexUrl, this.tokenName)
+      const hexColor = data.hex;
+      if (hexColor) {
+        square.style.backgroundColor = hexColor;
+      }
+      for (let itemIndex in this.currentGroupData) {
+        let item = this.currentGroupData[itemIndex];
+        for (let printItemIndex in item.print_item) {
+          let printItem = item.print_item[printItemIndex];
+          if (printItem.place === place) {
+            printItem.color[index]['pantone'] = pantone;
+          }
+        }
+      }
+    },
+    handleTotalOrientation() {
+      const firstItem = this.currentGroupData[0].print_item;
+      const orientationList = []
+      for (let index in firstItem) {
+        orientationList[index] = {
+          'position': firstItem[index]['position'],
+          'position_id': firstItem[index]['position_id'],
+          'image_id': firstItem[index]['image_id']
+        }
+      }
+      for (let i = 1; i < this.currentGroupData.length; i++) {
+        for (let j = 0; j < orientationList.length; j++) {
+          this.currentGroupData[i].print_item[j]['position_id'] = orientationList[j]['position_id'];
+          this.currentGroupData[i].print_item[j]['position'] = orientationList[j]['position'];
+          this.currentGroupData[i].print_item[j]['image_id'] = orientationList[j]['image_id'];
+        }
+      }
+      this.orientationResult = orientationList;
+    },
+  },
+  watch: {
+    totalOrientation: {
+      handler: 'handleTotalOrientation',
+    }
+  }
 }
 </script>
 
 <template>
+  <div class="note-frame">
+    <div class="note-frame__small"
+         :class="!showMiniature ? 'id-hidden' : ''"
+    >
+      <SingleNoteSmallComponent
+          v-for="(note, index) in currentGroupData"
+          :key="index"
+          :note-data="note"
+          :note-images="groupImages"
+          @print-item-checked="printItemChecked"
+          :select-all="selectAll"
+          :total-orientation="orientationResult"
+      />
+    </div>
+    <div class="note-frame__big">
+      <div v-for="item in Object.keys(bigImages)"
+           :key="item.id"
+      >
+        <div v-if="bigImages[item]" class="note-frame__big_center">
+          <div
+               v-html="bigImages[item][0].replace('itemWidth', bigImages[item][3])"
+          ></div>
+          <div >расположение:&nbsp;{{ bigImages[item][2] }}</div>
+        </div>
+      </div>
+      <div class="note-frame__big_footer"
+           v-for="(printItem, index) in currentGroupData[0].print_item"
+           :key="index">
+        <div><b>{{ printItem.position }}</b>&nbsp;<small>({{ printItem.place }})</small></div>
+        <div class="note-frame__big_footer"
+             v-for="(color, index) in printItem.color"
+             :key="index"
+        >
+          <div>цвет</div>
+          <div>{{ index + 1 }}</div>
+          <input
+              type="text"
+              class="note-frame__big_input"
+              @change="changeColor($event, printItem.place, index)"
+          >
+          <div class="note-frame__big_square"></div>
+        </div>
+      </div>
+    </div>
+
+  </div>
 
 </template>
 
 <style scoped lang="scss">
+@import "@/assets/maket/scss/vars";
+@import "@/assets/maket/scss/mixins";
 
+.note-frame {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  width: 100%;
+
+  &__big {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 16px;
+    padding: 12px 0;
+    width: 100%;
+
+    &_center {
+      text-align: center;
+    }
+
+    &_footer {
+      @include d-flex-center(center);
+      font-size: 14px;
+      padding: 8px 0;
+      gap: 8px;
+    }
+
+    &_input {
+      @include brd-standard;
+      padding: 6px;
+      max-width: 60px;
+    }
+
+    &_square {
+      @include brd-standard;
+      height: 30px;
+      width: 30px;
+    }
+  }
+
+  &__small {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 8px
+  }
+
+}
 </style>
---
+++ src/components/maket/maket_layout/layout_content/note_component/SingleNoteSmallComponent.vue
@@ -0,0 +1,279 @@
+<script>
+import {setImageColors} from "@/components/maket/maket_layout/layout_content/setImageColors.js";
+
+export default {
+  name: "SingleNoteSmallComponent",
+  inject: ['appUrl', 'tokenName'],
+  emits: ['print-item-checked'],
+  data() {
+    return {
+      currentNoteData: this.noteData,
+      article: this.noteData['article'],
+      colorImages: {},
+    }
+  },
+  props: {
+    noteData: Object,
+    noteImages: Array,
+    selectAll: Boolean,
+    totalOrientation: Array,
+  },
+  methods: {
+    async setColorImages() {
+      for (const image of this.noteImages) {
+        this.colorImages[image[0]] = [await setImageColors(this.article, image[1], this.appUrl, this.tokenName),
+          image[2], image[3], image[4]];
+      }
+    },
+    showImageList(event) {
+      console.log()
+      event.target.closest('.pen-small__single-item').querySelector('.image-list').style.display = 'block';
+    },
+    selectImage(event, image, printItem) {
+      const printItemId = printItem.id;
+      const selectedImage = this.colorImages[image]
+      event.target.closest('.image-list').style.display = 'none';
+      const itemFromList = this.currentNoteData.print_item.find(item => item.id === printItemId);
+      itemFromList['image_id'] = image;
+      itemFromList['position_id'] = selectedImage[1];
+      itemFromList['position'] = selectedImage[2];
+      if (event.target.closest('.pen-small__single-item').querySelector('.check').checked) {
+        this.$emit('print-item-checked', [itemFromList.id, this.colorImages[itemFromList.image_id]])
+      }
+    },
+    printItemChecked(event, printItem) {
+      if (event.checked) {
+        this.$emit('print-item-checked', [printItem.id, this.colorImages[printItem.image_id]])
+      } else {
+        this.$emit('print-item-checked', [printItem.id, null])
+      }
+    },
+    selectAllHandler() {
+      const block = document.getElementById(`${this.currentNoteData['id']}`);
+      block.querySelectorAll('.check').forEach(el => {
+        el.checked = this.selectAll;
+        const printItem = this.currentNoteData.print_item.find(item => item.id === Number(el.id));
+        this.printItemChecked(el, printItem);
+      });
+    },
+    setAllPositions() {
+      this.currentNoteData.print_item.forEach(itemFromList => {
+        const selectedImage = this.colorImages[itemFromList['image_id']]
+        itemFromList['position_id'] = selectedImage[1];
+        itemFromList['position'] = selectedImage[2];
+      });
+    },
+    setAllPositionChanged() {
+      this.currentNoteData.print_item.forEach(itemFromList => {
+        const checkBoxValue = document.getElementById(itemFromList.id).checked;
+        if (checkBoxValue) {
+          this.$emit('print-item-checked', [itemFromList.id, this.colorImages[itemFromList.image_id]])
+        }
+      });
+    },
+    changePrintItemColor(event, printItem, index){
+      printItem.color[index]['pantone'] = event.target.value;
+    }
+  },
+  created() {
+    (async () => {
+      await this.setColorImages();
+      this.setAllPositions();
+    })();
+  },
+  watch: {
+    selectAll: {
+      handler: 'selectAllHandler'
+    },
+    totalOrientation: {
+      handler: 'setAllPositionChanged'
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="note-small"
+       :id="currentNoteData.id"
+  >
+    <div class="active">{{ currentNoteData['no'] }}</div>
+    <div class="note-small__prints">
+      <div class="note-small__single-item"
+           v-for="printItem in currentNoteData.print_item"
+           :key="printItem"
+      >
+        <div class="note-small__item">
+          <div class="note-small__image"
+               v-html="colorImages[printItem.image_id] ? colorImages[printItem.image_id][0].replace('itemWidth', '20') : colorImages"
+          >
+          </div>
+        </div>
+        <div class="note-small__text">
+          <input
+              type="checkbox"
+              class="check"
+              :id="printItem.id"
+              @change="printItemChecked($event.target, printItem)"
+          >&nbsp;
+          <label :for="printItem.id"
+                 class="pen-small__wrap"
+          >
+            {{ article }}</label>
+        </div>
+        <div class="note-small__tech no-print">
+          <div
+              v-if="colorImages[printItem.image_id]"
+              @click="printItem.image_list.length > 1 ? showImageList($event) : null"
+              :data-id="printItem.image_id"
+          >
+            <div class="note-small__dropdown">
+              <div class="image-list__nowrap">{{ colorImages[printItem.image_id][2] }}</div>
+              &nbsp;
+              <font-awesome-icon
+                  v-if="printItem.image_list.length > 1"
+                  :icon="['fas', 'chevron-down']"/>
+            </div>
+          </div>
+          <div
+              v-for="(color, index) in printItem.color"
+              :key="index"
+              class="note-small__color-set"
+              @keydown.enter="changePrintItemColor($event, printItem, index)"
+          >
+            <div>{{ index + 1 }}</div>
+            <input type="text"
+                   class="note-small__color"
+                   :value="color['pantone']"
+            >
+          </div>
+          <ul class="image-list">
+            <li class="image-list__nowrap"
+                v-for="image in printItem.image_list"
+                :key="image.id"
+                :data-id="image"
+                @click="selectImage($event, image, printItem)"
+            >
+              <div v-if="colorImages[image]">{{ colorImages[image][2] }}</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "@/assets/maket/scss/vars";
+@import "@/assets/maket/scss/mixins";
+
+.note-small {
+  @include d-flex-center(flex-start);
+  font-size: 8px;
+
+  &__prints {
+    display: flex;
+    width: 100%;
+  }
+
+  &__item {
+    padding: 3px 0;
+    display: flex;
+    align-items: center;
+    gap: 3px;
+    position: relative;
+  }
+
+  &__tech {
+    border: 1px solid $colorPrimary800;
+    border-radius: 10px;
+    font-size: 12px;
+    padding: 6px;
+    position: absolute;
+    z-index: 1;
+    left: 100%;
+    top: 28mm;
+    @include d-flex-center(space-between);
+    gap: 12px;
+    transition: opacity 0.5s ease-in-out, visibility 0.5s ease-in-out;
+    background-color: $colorPrimary200;
+    opacity: 0;
+    visibility: hidden;
+    transition-delay: 0.2s;
+  }
+
+  &__single-item {
+    padding: 3px 3px 3px 6px;
+    position: relative;
+    @include d-flex-center(space-between);
+    flex-direction: column;
+    gap: 4px;
+
+    &:hover .note-small__tech {
+      transition: opacity 0.5s ease-in-out, visibility 0.5s ease-in-out;
+      transition-delay: 0.2s;
+      opacity: 1;
+      visibility: visible;
+    }
+  }
+
+  &__text {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  &__dropdown {
+    @include d-flex-center(space-between);
+    padding: 3px;
+    @include brd-standard;
+    border: transparent;
+
+    &:hover {
+      background-color: $colorPrimary100;
+    }
+  }
+
+  &__color-set {
+    display: flex;
+    align-items: center;
+    gap: 4px;
+  }
+
+  &__color {
+    @include brd-standard;
+    max-width: 60px;
+    font-size: 12px;
+    padding: 4px 10px;
+  }
+
+  &__wrap {
+    text-overflow: ellipsis;
+  }
+
+}
+
+.image-list {
+  display: none;
+  position: absolute;
+  list-style: none;
+  background-color: white;
+  top: 1rem;
+  z-index: 5;
+  @include brd-standard;
+
+  & li {
+    padding: 4px 1mm;
+    border: solid 0.5px transparent;
+    border-radius: 6px;
+  }
+
+  & li:hover {
+    background-color: $colorSecondary100;
+  }
+
+  &__nowrap {
+    text-wrap: nowrap;
+  }
+}
+
+</style>
