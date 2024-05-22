dd92cad9734d2d18b40bffa8fb8a1d77eb2546ff
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-05-22 22:21:19 +0300

    dropdown to select maket

--- src/components/maket/maket_layout/layout_settings/TechDataComponent.vue
+++ src/components/maket/maket_layout/layout_settings/TechDataComponent.vue
@@ -1,10 +1,10 @@
 <script>
 
-import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 export default {
-  name: "TechDataComponent",
-  components: {FontAwesomeIcon},
+  name: 'TechDataComponent',
+  components: { FontAwesomeIcon },
   emits: [
     'save-maket',
     'show-sort',
@@ -14,10 +14,11 @@ export default {
     'tech-info-changed'
   ],
   props: {
+    orderId: String,
     maketId: String,
     formatList: Object,
     formatSelected: Number,
-    techInfo: Object,
+    techInfo: Object
   },
   data() {
     return {
@@ -25,21 +26,21 @@ export default {
     }
   },
   methods: {
-    saveMaket(){
-      this.$emit('save-maket');
+    saveMaket() {
+      this.$emit('save-maket')
     },
     togglePictures() {
-      this.currentTechInfo['pictureShow'] = !this.currentTechInfo['pictureShow'];
-      this.$emit('tech-info-changed', this.currentTechInfo);
+      this.currentTechInfo['pictureShow'] = !this.currentTechInfo['pictureShow']
+      this.$emit('tech-info-changed', this.currentTechInfo)
 
     },
     toggleFrame() {
-      this.currentTechInfo['frameShow'] = !this.currentTechInfo['frameShow'];
-      this.$emit('tech-info-changed', this.currentTechInfo);
+      this.currentTechInfo['frameShow'] = !this.currentTechInfo['frameShow']
+      this.$emit('tech-info-changed', this.currentTechInfo)
     },
     toggleTable() {
-      this.currentTechInfo['tableShow'] = !this.currentTechInfo['tableShow'];
-      this.$emit('tech-info-changed', this.currentTechInfo);
+      this.currentTechInfo['tableShow'] = !this.currentTechInfo['tableShow']
+      this.$emit('tech-info-changed', this.currentTechInfo)
     },
     showSort() {
       this.$emit('show-sort')
@@ -54,23 +55,28 @@ export default {
       this.$emit('window-close')
     },
     showFormatList(event) {
-      event.target.closest('.format__block').querySelector('.format__list').style.display = 'block';
+      event.target.closest('.format__block').querySelector('.format__list').style.display = 'block'
     },
     selectFormat(event, item) {
-      event.preventDefault();
-      event.target.closest('.format__list').style.display = 'none';
-      this.currentTechInfo['formatSelected'] = item;
-      this.$emit('tech-info-changed', this.currentTechInfo);
-    }
+      event.preventDefault()
+      event.target.closest('.format__list').style.display = 'none'
+      this.currentTechInfo['formatSelected'] = item
+      this.$emit('tech-info-changed', this.currentTechInfo)
+    },
+    selectMaket(maketId, orderId){
+      if(Number(maketId) !== Number(this.maketId)) {
+        window.location.href = `http://localhost:5173/maket/layout?maketId=${maketId}&orderId=${orderId}`;
+      }
+    },
   },
   watch: {
     techInfo: {
       immediate: true,
       handler(newVal) {
-        this.currentTechInfo = newVal;
+        this.currentTechInfo = newVal
       }
     }
-  },
+  }
 }
 </script>
 
@@ -96,7 +102,7 @@ export default {
           {{ formatList[currentTechInfo['formatSelected']][0] }}
         </div>
         &nbsp;
-        <font-awesome-icon :icon="['fas', 'chevron-down']"/>
+        <font-awesome-icon :icon="['fas', 'chevron-down']" />
         <ul class="format__list">
           <li v-for="item in Object.keys(formatList)"
               :key="item"
@@ -109,12 +115,12 @@ export default {
       </div>
       &emsp;
       <input
-          v-if="currentTechInfo"
-          type="checkbox"
-          class="check"
-          id="table"
-          @change="toggleTable"
-          :checked="currentTechInfo['tableShow']"
+        v-if="currentTechInfo"
+        type="checkbox"
+        class="check"
+        id="table"
+        @change="toggleTable"
+        :checked="currentTechInfo['tableShow']"
       >
       &nbsp;
       <label for="table"
@@ -122,12 +128,12 @@ export default {
     </div>
     <div class="blank-header__item">
       <input
-          v-if="currentTechInfo"
-          type="checkbox"
-          class="check"
-          id="pictures"
-          @change="togglePictures"
-          :checked="currentTechInfo['pictureShow']"
+        v-if="currentTechInfo"
+        type="checkbox"
+        class="check"
+        id="pictures"
+        @change="togglePictures"
+        :checked="currentTechInfo['pictureShow']"
       >
       &nbsp;
       <label class="check"
@@ -135,12 +141,12 @@ export default {
     </div>
     <div class="blank-header__item">
       <input
-          v-if="currentTechInfo"
-          type="checkbox"
-          class="check"
-          id="frames"
-          @change="toggleFrame"
-          :checked="currentTechInfo['frameShow']"
+        v-if="currentTechInfo"
+        type="checkbox"
+        class="check"
+        id="frames"
+        @change="toggleFrame"
+        :checked="currentTechInfo['frameShow']"
       >
       &nbsp;
       <label for="frames"
@@ -156,23 +162,46 @@ export default {
     </button>
     <div></div>
     <input
-        v-if="currentTechInfo"
-        type="number"
-        class="form-input"
-        placeholder="номер макета"
-        v-model="currentTechInfo['maketNumber']"
+      v-if="currentTechInfo"
+      type="number"
+      class="form-input"
+      placeholder="номер макета"
+      v-model="currentTechInfo['maketNumber']"
     >
     <input
-        v-if="currentTechInfo"
-        type="text"
-        class="form-input long-input"
-        placeholder="комментарий к макету"
-        v-model="currentTechInfo['maketComment']">
+      v-if="currentTechInfo"
+      type="text"
+      class="form-input long-input"
+      placeholder="комментарий к макету"
+      v-model="currentTechInfo['maketComment']">
     <button
-        class="btn btn-save"
-        @click="saveMaket"
-    >сохранить&nbsp;макет</button>
-    <button class="btn btn-neutral" v-if="maketId !== '0'">новый&nbsp;макет</button>
+      class="btn btn-save"
+      @click="saveMaket"
+    >сохранить&nbsp;макет
+    </button>
+    <div class="btn btn-neutral maket-btn">
+      <div class="maket-btn__title">
+        <div>
+          {{ currentTechInfo.maketId ? currentTechInfo.maketNumber
+          + '\u00A0от\u00A0' + currentTechInfo.maketDate : 'новый\u00A0макет' }}
+        </div>
+        <font-awesome-icon :icon="['fas', 'chevron-down']" />
+      </div>
+      <ul class="maket-list">
+        <li value="0"
+            @click="selectMaket('0', orderId)"
+            v-show="Number(maketId) !== 0"
+        >новый&nbsp;макет</li>
+        <li
+          v-for="maket in currentTechInfo['maketData']"
+          :key="maket.id"
+          value="maket.Id"
+          v-show="Number(maketId) !== Number(maket.id)"
+          @click="selectMaket(maket.id, orderId, $event)"
+        >{{ maket.maket_number }}&nbsp;от&nbsp;{{ maket.date }}
+        </li>
+      </ul>
+    </div>
   </header>
 
 </template>
@@ -249,6 +278,40 @@ export default {
         background-color: $colorPrimary100;
       }
 
+    }
+  }
+}
+
+.maket-btn {
+
+  &__title {
+    display: flex;
+    gap: 12px;
+    align-items: center;
+  }
+
+  &:hover .maket-list {
+    display: block;
+  }
+}
+
+.maket-list {
+  display: none;
+  position: absolute;
+  list-style: none;
+  background-color: white;
+  @include brd-standard;
+  top: 3rem;
+  right: 10px;
+
+  & li {
+    min-width: 152px;
+    padding: 7px 20px 7px 12px;
+
+    &:hover {
+      background-color: $colorPrimary100;
+      @include brd-standard;
+      border-color: transparent;
     }
   }
 }
