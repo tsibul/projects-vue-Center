bc396a3f506b93820e094bff444fa76681bd04be
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-06-05 20:59:30 +0300

    limit table size wiyh overflow-y

--- src/components/maket/film/FilmComponent.vue
+++ src/components/maket/film/FilmComponent.vue
@@ -8,7 +8,7 @@ export default {
   inject: ['appUrl', 'tokenName'],
   components: {
     FilmSingleComponent,
-    ClauseHeadingComponent,
+    ClauseHeadingComponent
   },
   data() {
     return {
@@ -23,46 +23,46 @@ export default {
   methods: {
     search(searchInput) {
       if (searchInput !== '') {
-        this.searchString = searchInput.replace(' ', '_');
+        this.searchString = searchInput.replace(' ', '_')
       } else {
-        this.searchString = 'default';
+        this.searchString = 'default'
       }
-      this.idLast = 0;
-      this.filmList = [];
-      this.addNextRecords();
+      this.idLast = 0
+      this.filmList = []
+      this.addNextRecords()
     },
     hideDeletedChange(checked) {
-      this.showDeleted = checked;
+      this.showDeleted = checked
     },
     async allFilms() {
-      const filmListUrl = `${this.appUrl}film_list/${this.searchString}/${this.showDeleted ? 1 : 0}/${this.idLast}`;
-      return await fetchData(filmListUrl, this.tokenName);
+      const filmListUrl = `${this.appUrl}film_list/${this.searchString}/${this.showDeleted ? 1 : 0}/${this.idLast}`
+      return await fetchData(filmListUrl, this.tokenName)
     },
     addNextRecords() {
       this.allFilms().then(newData => {
         if (newData.length) {
-          this.idLast = this.idLast + 20;
+          this.idLast = this.idLast + 20
           if (this.filmList) {
-            this.filmList = [...this.filmList, ...newData];
+            this.filmList = [...this.filmList, ...newData]
           } else {
-            this.filmList = newData;
+            this.filmList = newData
           }
         }
       })
     },
     hideDeletedChecked() {
-      this.idLast = 0;
-      this.filmList = [];
-      this.addNextRecords();
-    },
+      this.idLast = 0
+      this.filmList = []
+      this.addNextRecords()
+    }
   },
   watch: {
     showDeleted: {
-      handler: 'hideDeletedChecked',
+      handler: 'hideDeletedChecked'
     }
   },
   created() {
-    this.addNextRecords();
+    this.addNextRecords()
   }
 }
 </script>
@@ -79,21 +79,23 @@ export default {
       @search-string="search"
       @hide-deleted-change="hideDeletedChange"
     />
-    <div class="film__row active ">
-      <div>номер&nbsp;</div>
-      <div>дата</div>
-      <div>дата отправки</div>
-      <div>формат</div>
-      <div>файл</div>
-      <div>статус</div>
-      <div></div>
-      <button type="button" class="btn btn-save">новая&nbsp;пленка</button>
+    <div class="film__list">
+      <div class="film__row active ">
+        <div>номер&nbsp;</div>
+        <div>дата</div>
+        <div>дата отправки</div>
+        <div>формат</div>
+        <div>файл</div>
+        <div>статус</div>
+        <div></div>
+        <button type="button" class="btn btn-save">новая&nbsp;пленка</button>
+      </div>
+      <FilmSingleComponent
+        v-for="(film, index) in filmList"
+        :key="index"
+        :film="film"
+      />
     </div>
-    <FilmSingleComponent
-      v-for="(film, index) in filmList"
-      :key="index"
-      :film="film"
-    />
   </div>
 </template>
 
@@ -113,6 +115,11 @@ export default {
     flex-wrap: nowrap;
     grid-template-columns: 1fr 2fr 2fr 2fr 6fr 1fr 2fr 2fr;
     margin-right: 19px;
+  }
+
+  &__list {
+    max-height: calc(100vh - 170px);
+    overflow-y: auto;
   }
 }
 </style>
