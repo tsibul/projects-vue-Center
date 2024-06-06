a06efdc7341aefc4c1d75282c47ec7d38adaa81e
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-06-06 15:21:33 +0300

    reformat date from dd.mm.yy to YYYY-mm-dd

--- src/components/services/reformatDate.js
+++ src/components/services/reformatDate.js
@@ -1,2 +1,11 @@
 'use strict';
 
+export function stringToDate(string){
+  let dateString;
+  if (string) {
+    const dateParts = string.split('.');
+    dateString = `20${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`;
+  }
+  return dateString;
+}
+
