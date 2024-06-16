349d09183bd98366eaf90cb247c84739b9e4ffef
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2024-06-16 16:17:13 +0300

    button menu show shows menu on hover

--- src/components/menu/MenuComponent.vue
+++ src/components/menu/MenuComponent.vue
@@ -1,21 +1,24 @@
 <script setup>
 
-import MenuLeftComponent from "@/components/menu/MenuLeftComponent.vue";
-import MenuRightComponent from "@/components/menu/MenuRightComponent.vue";
+import MenuLeftComponent from '@/components/menu/MenuLeftComponent.vue'
+import MenuRightComponent from '@/components/menu/MenuRightComponent.vue'
 </script>
 
 <template>
-  <header class="header">
-    <div class="container">
-      <menu class="menu">
-        <MenuLeftComponent
+  <div class="menu-show">
+<!--    <font-awesome-icon :icon="['fas', 'bars']" />-->
+    Меню
+    <header class="header header__hidden">
+      <div class="container">
+        <menu class="menu">
+          <MenuLeftComponent
             :menuItems="menuItems"
-            @item-selected="handleItemSelected"/>
-        <!--        <MenuRightComponent :currentUser="currentUser" />-->
-        <MenuRightComponent/>
-      </menu>
-    </div>
-  </header>
+            @item-selected="handleItemSelected" />
+          <MenuRightComponent />
+        </menu>
+      </div>
+    </header>
+  </div>
 </template>
 <script>
 export default {
@@ -26,17 +29,51 @@ export default {
     }
   },
   props: {
-    menuItems: Object,
+    menuItems: Object
   },
   methods: {
     handleItemSelected(selectedItem) {
-      this.selectedItem = selectedItem;
-      this.$emit('item-selected', this.selectedItem);
+      this.selectedItem = selectedItem
+      this.$emit('item-selected', this.selectedItem)
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
+@import '@/assets/maket/scss/vars';
+@import '@/assets/maket/scss/mixins';
+
+.menu-show {
+  @include brd-standard;
+  border-color: transparent;
+  color: $colorPrimary50;
+  position: fixed;
+  top: 2px;
+  z-index: 15;
+  //font-weight: 600;
+  padding: 19px 8px;
+  left: calc((100vw - 1600px) / 2 - 72px);
+  background-color: $colorSecondary400;
+  //font-size: 38px;
+  //padding: 6px 12px;
+  //left: calc((100vw - 1600px) / 2 - 64px);
+
+  &:hover .header__hidden {
+    display: block;
+  }
+}
+
+.header__hidden {
+  display: none;
+  top: 0;
+  left: 0;
+}
+
+@media (max-width: 1600px) {
+  .menu-show {
+    left: 8px;
+  }
+}
 
 </style>
